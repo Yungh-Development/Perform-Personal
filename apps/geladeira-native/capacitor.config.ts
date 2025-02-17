7a9ca4a9d1ea1b56007b9fdd78cae2363c634fd8
Diego Yungh <diego.yungh@gmail.com>
2025-02-17 11:59:43 -0300

    Initial commit

---
+++ apps/geladeira-native/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'geladeira-native',
+  webDir: 'dist'
+};
+
+export default config;
