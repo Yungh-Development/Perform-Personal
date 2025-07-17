4e3bb3a98dc4f7758e2905a779a9c5dbbfa691cc
Monday <mgabriel.moliveira@gmail.com>
2025-07-17 10:34:54 -0300

    Adicionado breve tutorial e adicionado nova estilização

--- apps/ionic-app/src/pages/home/page.tsx
+++ apps/ionic-app/src/pages/home/page.tsx
@@ -1,12 +1,56 @@
-import { IonContent, IonPage } from '@ionic/react';
+import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import FitnessCenterOutlinedIcon from '@mui/icons-material/FitnessCenterOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import ListAltOutlinedIcon from '@mui/icons-material/ListAltOutlined';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 import FooterTemplate from '../template/footer/page';
 
 
 const HomePage = () => {
   return (
-    <IonPage>     
-      <IonContent color="page-background" className="ion-padding">
-        <h1>HOMEPAGE</h1>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar className="bg-blue-600">
+          <div className="flex ion-padding my-2">    
+            <div className="flex ml-4 justify-center items-center">
+              <IonTitle className="text-white text-2xl">Homepage</IonTitle>
+            </div>
+          </div>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonList>
+          <IonItem>
+            <HomeOutlinedIcon className="mr-4"/>
+            <IonLabel>  
+              <h3>Botão para a HomePage</h3>
+            </IonLabel>
+          </IonItem>
+        </IonList>
+        <IonList>
+          <IonItem>
+            <FitnessCenterOutlinedIcon className="mr-4"/>
+            <IonLabel>  
+              <h3>Botão para Relatório de Treinos</h3>
+            </IonLabel>
+          </IonItem>
+        </IonList>
+        <IonList>
+          <IonItem>
+            <AddOutlinedIcon className="mr-4"/>
+            <IonLabel>  
+              <h3 className='ml-4'>Botão para Adicionar Aluno e Treino</h3>
+            </IonLabel>
+          </IonItem>
+        </IonList>
+        <IonList>
+          <IonItem>
+            <ListAltOutlinedIcon className="mr-4"/>
+            <IonLabel>  
+              <h3>Botão para Lista de Alunos Cadastrados</h3>
+            </IonLabel>
+          </IonItem>
+        </IonList>      
       </IonContent>
       <FooterTemplate/>
     </IonPage>
--- apps/ionic-app/src/pages/template/footer/page.tsx
+++ apps/ionic-app/src/pages/template/footer/page.tsx
@@ -1,36 +1,56 @@
 import { IonButton, IonFooter } from '@ionic/react';
-
+import FitnessCenterOutlinedIcon from '@mui/icons-material/FitnessCenterOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import ListAltOutlinedIcon from '@mui/icons-material/ListAltOutlined';
+import { useLocation } from 'react-router-dom';
 
 const FooterTemplate = () => {
+  const location = useLocation();
+
   return (
-    <IonFooter className="relative">
-      <div className="absolute bg-amber-500 rounded-full w-14 h-14 flex items-center justify-center left-1/2 -ml-7 -top-1/2">
+    <IonFooter className="relative bg-[#1F1F1F]" >
+      <div className="absolute bg-amber-500 rounded-full w-12 h-12 flex items-center justify-center left-1/2 -ml-6 -top-1/2">
         <div className="flex">
-        <a href="/formulario-performance" className="mt-2">
-          <ion-icon name="add" color="black" size="large"></ion-icon>
-        </a>
+          <a href="/formulario-performance" className="mt-2">
+            <ion-icon name="add" color="black" size="large"></ion-icon>
+          </a>
         </div>
       </div>
       <div className="flex items-center py-4">
         <div className="flex justify-around w-full">
-        <IonButton fill="clear">
-          <a href="/home">
-            <ion-icon name="grid" size="large"></ion-icon>
-          </a>
-        </IonButton>       
-        <IonButton fill="clear">
-          <a href="/relatorio">
-            <ion-icon name="pie" size="large"></ion-icon>
-          </a>
-        </IonButton>
+          <IonButton
+            fill="clear"
+            className={location.pathname === '/home' ? 'bg-amber-500 rounded-md h-4 w-10' : ''}
+          >
+            <a href="/home">
+              <HomeOutlinedIcon
+                sx={{ fontSize: 25, color: location.pathname === '/home' ? 'white' : 'gray' }}
+              />
+            </a>
+          </IonButton>
+          <IonButton
+            fill="clear"
+            className={location.pathname === '/relatorio' ? 'bg-amber-500 rounded-md h-4 w-10' : ''}
+          >
+            <a href="/relatorio">
+              <FitnessCenterOutlinedIcon
+                sx={{ fontSize: 25, color: location.pathname === '/relatorio' ? 'white' : 'gray' }}
+              />
+            </a>
+          </IonButton>
         </div>
-        <div className="w-full flex justify-center">      
-        <IonButton fill="clear">
-          <a href="/lista-alunos">
-            <ion-icon name="people" size="large"></ion-icon>
-          </a>
-        </IonButton>  
-        </div>  
+        <div className="w-full flex justify-center">
+          <IonButton
+            fill="clear"
+            className={location.pathname === '/lista-alunos' ? 'bg-amber-500 rounded-md h-4 w-10' : ''}
+          >
+            <a href="/lista-alunos">
+              <ListAltOutlinedIcon
+                sx={{ fontSize: 25, color: location.pathname === '/lista-alunos' ? 'white' : 'gray' }}
+              />
+            </a>
+          </IonButton>
+        </div>
       </div>
     </IonFooter>
   );
